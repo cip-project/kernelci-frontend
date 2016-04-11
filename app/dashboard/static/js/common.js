6bc4b4f6a2964402ad7db34f6083ef85818505d8
Milo Casagrande <milo.casagrande@linaro.org>
2016-04-11 14:48:00 +0200

    Add initial structure for test suite view

--- app/dashboard/static/js/common.js
+++ app/dashboard/static/js/common.js
@@ -53,7 +53,8 @@ require.config({
             'app/view-socs-soc-job-kernel': 'app/view-socs-soc-job-kernel.2016.3.2',
             'app/view-sponsors': 'app/view-sponsors.2016.3.2',
             'app/view-stats': 'app/view-stats.2016.3.2',
-            'app/view-tests-all': 'app/view-tests-all.2016.3.1'
+            'app/view-tests-all': 'app/view-tests-all.2016.3.1',
+            'app/view-tests-suite': 'app/view-tests-suite.2016.3.1'
         }
     },
     shim: {
