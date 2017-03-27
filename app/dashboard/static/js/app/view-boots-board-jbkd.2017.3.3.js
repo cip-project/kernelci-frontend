52b71ee1ed38883e37dbd84fc9aa7c0ef5dc6729
Milo Casagrande <milo.casagrande@linaro.org>
2017-03-27 09:44:23 +0200

    Add missing view

---
+++ app/dashboard/static/js/app/view-boots-board-jbkd.2017.3.3.js
@@ -0,0 +1,270 @@
+/*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
+require([
+    'jquery',
+    'utils/init',
+    'utils/error',
+    'utils/request',
+    'utils/html',
+    'tables/boot',
+    'utils/table'
+], function($, init, e, r, html, tboot, table) {
+    'use strict';
+    var gBootsTable;
+    var fileServer;
+    var gBoard;
+    var gBranch;
+    var gDefconfigFull;
+    var gKernel;
+    var gTree;
+
+    setTimeout(function() {
+        document.getElementById('li-boot').setAttribute('class', 'active');
+    }, 10);
+
+    function getBootsFail() {
+        html.removeElement(
+            document.getElementById('boot-reports-loading-div'));
+        html.replaceContent(
+            document.getElementById('other-reports-table-div'),
+            html.errorDiv('Error loading data.'));
+    }
+
+    function getBootsDone(response) {
+        var columns;
+        var results;
+        var rowURLFmt;
+
+        /**
+         * Wrapper to inject the server URL.
+        **/
+        function _renderBootLogs(data, type, object) {
+            object.default_file_server = fileServer;
+            return tboot.renderBootLogs(data, type, object);
+        }
+
+        results = response.result;
+        if (results.length === 0) {
+            html.removeElement(
+                document.getElementById('table-loading'));
+            html.replaceContent(
+                document.getElementById('table-div'),
+                html.errorDiv('No data found.'));
+        } else {
+            rowURLFmt = '/boot/id/%(_id)s/';
+
+            columns = [
+                {
+                    data: 'lab_name',
+                    title: 'Lab Name',
+                    type: 'string',
+                    className: 'lab-column',
+                    render: tboot.renderLab
+                },
+                {
+                    data: 'arch',
+                    title: 'Arch.',
+                    type: 'string',
+                    className: 'arch-column'
+                },
+                {
+                    data: 'boot_result_description',
+                    title: 'Failure Reason',
+                    type: 'string',
+                    className: 'failure-column',
+                    render: tboot.renderResultDescription
+                },
+                {
+                    data: 'file_server_url',
+                    title: 'Boot Log',
+                    type: 'string',
+                    className: 'log-column pull-center',
+                    render: _renderBootLogs
+                },
+                {
+                    data: 'created_on',
+                    title: 'Date',
+                    type: 'date',
+                    className: 'date-column pull-center',
+                    render: tboot.renderDate
+                },
+                {
+                    data: 'status',
+                    title: 'Status',
+                    type: 'string',
+                    className: 'pull-center',
+                    render: tboot.renderStatus
+                },
+                {
+                    data: '_id',
+                    title: '',
+                    type: 'string',
+                    orderable: false,
+                    searchable: false,
+                    className: 'select-column pull-center',
+                    render: tboot.renderDetails
+                }
+            ];
+
+            gBootsTable
+                .data(results)
+                .columns(columns)
+                .order([4, 'desc'])
+                .rowURL(rowURLFmt)
+                .rowURLElements(['_id'])
+                .draw();
+        }
+    }
+
+    function getBoots() {
+        var deferred;
+
+        deferred = r.get(
+            '/_ajax/boot',
+            {
+                board: gBoard,
+                job: gTree,
+                git_branch: gBranch,
+                kernel: gKernel,
+                defconfig_full: gDefconfigFull
+            }
+        );
+        $.when(deferred)
+            .fail(e.error, getBootsFail)
+            .done(getBootsDone);
+    }
+
+    function setUpData() {
+        var aNode;
+        var spanNode;
+        var str;
+        var tooltipNode;
+
+        // Add the tree data.
+        spanNode = document.createElement('span');
+
+        tooltipNode = html.tooltip();
+        str = 'Boot details for&nbsp;';
+        str += gTree;
+        tooltipNode.setAttribute('title', str);
+
+        aNode = document.createElement('a');
+        str = '/boot/all/job/';
+        str += gTree;
+        str += '/';
+        aNode.setAttribute('href', str);
+        aNode.appendChild(document.createTextNode(gTree));
+
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        spanNode.insertAdjacentHTML('beforeend', '&nbsp;&mdash;&nbsp;');
+
+        tooltipNode = html.tooltip();
+        str = 'Details for job&nbsp;';
+        str += gTree;
+        tooltipNode.setAttribute('title', str);
+
+        aNode = document.createElement('a');
+        str = '/job/';
+        str += gTree;
+        str += '/';
+        aNode.setAttribute('href', str);
+
+        aNode.appendChild(html.tree());
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        html.replaceContent(document.getElementById('dd-tree'), spanNode);
+
+        // Add the git branch data.
+        html.replaceContent(
+            document.getElementById('dd-branch'),
+            document.createTextNode(gBranch));
+
+        // Add the kernel data.
+        spanNode = document.createElement('span');
+
+        tooltipNode = html.tooltip();
+        str = 'Boot reports for&nbsp;';
+        str += gTree;
+        str += '&nbsp;&ndash;&nbsp;';
+        str += gKernel;
+        tooltipNode.setAttribute('title', str);
+
+        aNode = document.createElement('a');
+        str = '/boot/all/job/';
+        str += gTree;
+        str += '/branch/';
+        str += gBranch;
+        str += '/kernel/';
+        str += gKernel;
+        str += '/';
+        aNode.setAttribute('href', str);
+        aNode.appendChild(document.createTextNode(gKernel));
+
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        spanNode.insertAdjacentHTML('beforeend', '&nbsp;&mdash;&nbsp;');
+
+        tooltipNode = html.tooltip();
+        str = 'Build reports for&nbsp;';
+        str += gTree;
+        str += '&nbsp;&ndash;&nbsp;';
+        str += gKernel;
+        tooltipNode.setAttribute('title', str);
+
+        aNode = document.createElement('a');
+        str = '/build/';
+        str += gTree;
+        str += '/branch/';
+        str += gBranch;
+        str += '/kernel/';
+        str += gKernel;
+        str += '/';
+        aNode.setAttribute('href', str);
+
+        aNode.appendChild(html.build());
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        html.replaceContent(
+            document.getElementById('dd-git-describe'), spanNode);
+
+        // Add the defconfig data.
+        html.replaceContent(
+            document.getElementById('dd-defconfig'),
+            document.createTextNode(gDefconfigFull));
+    }
+
+    if (document.getElementById('board-name') !== null) {
+        gBoard = document.getElementById('board-name').value;
+    }
+    if (document.getElementById('defconfig-full') !== null) {
+        gDefconfigFull = document.getElementById('defconfig-full').value;
+    }
+    if (document.getElementById('kernel-name') !== null) {
+        gKernel = document.getElementById('kernel-name').value;
+    }
+    if (document.getElementById('job-name') !== null) {
+        gTree = document.getElementById('job-name').value;
+    }
+    if (document.getElementById('branch-name') !== null) {
+        gBranch = document.getElementById('branch-name').value;
+    }
+    if (document.getElementById('file-server') !== null) {
+        fileServer = document.getElementById('file-server').value;
+    }
+
+    gBootsTable = table({
+        tableId: 'boots-table',
+        tableLoadingDivId: 'table-loading',
+        tableDivId: 'table-div'
+    });
+
+    setTimeout(setUpData, 25);
+    setTimeout(getBoots, 25);
+
+    setTimeout(init.hotkeys, 50);
+    setTimeout(init.tooltip, 50);
+});
